8151ca279319fef109e99ba428f58e7f1fc12144
Elliot Fiske <elliotfiske@gmail.com>
2017-07-28 22:16:29 -0700

    More tooltips for your drooltips

---
+++ Assets/E_Scripts/ShowTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTooltip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public GameObject tooltip;
+
+	public void RevealTooltip() {
+		tooltip.SetActive (true);
+	}
+
+	public void HideTooltip() {
+		tooltip.SetActive (false);
+	}
+}

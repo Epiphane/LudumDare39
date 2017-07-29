c6a82912953da223940f1b94dcf6345eddb466c7
Epiphane <exyphnos@gmail.com>
2017-07-29 01:58:31 -0700

    Playing with perspectives

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -6,29 +6,18 @@ public class CameraFollower : MonoBehaviour {
 
     public Transform toFollow;
     public float smooth;
+    private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		
+        offset = transform.position - toFollow.position;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Vector3 destination = toFollow.position;
+        Vector3 destination = toFollow.position + offset;
         Vector3 position = transform.position;
-        destination.y = position.y;
-
-        //if ((destination.z - position.z < .05) && (destination.z - position.z > 0)) {
-        //    moveTheCamera = false;
-        //} else if (moveTheCamera == true) {
-        //if (Mathf.Approximately(transform.eulerAngles.y, 0)) {
-        //    targetPosition2 = (transform.position + (targetPosition * -5));
-        //}
-        //if (Mathf.Approximately(transform.eulerAngles.y, 180)) {
-        //    targetPosition2 = (transform.position + (targetPosition * -5));
-        //}
         
         transform.position = Vector3.Lerp(position, destination, smooth * Time.deltaTime);
-        //}
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 15.0f;
+    private float delay = 0.3f;
+    private float clickTime = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 15.0f;
+    public float accel = 1.0f;
+    private float currentSpeed = 0;
+    public float maxSpeed = 15.0f;
+    private Vector3 velocity;
+
+    //private NavMeshAgent navMeshAgent;
+    private Transform targetedEnemy;
+    private bool walking;
+    private bool enemyClicked;
+    private Vector3 destination;
+
+    // Use this for initialization
+    void Start () {
+        //navMeshAgent = GetComponent<NavMeshAgent>();
+        velocity = Vector3.zero;
+    }
+
+    private string Move = "Fire2";
+
+    void IssueMove() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        int layer_mask = LayerMask.GetMask("Ground");
+        if (Physics.Raycast(ray, out hit, 100, layer_mask)) {
+            if (hit.collider.CompareTag("Enemy")) {
+                targetedEnemy = hit.transform;
+                enemyClicked = true;
+            } else {
+                walking = true;
+                enemyClicked = false;
+                destination = hit.point;
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Vector3 direction = speed * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetButtonDown(Move)) {
+            clickTime = Time.time;
+            IssueMove();
+        }
+        if (Input.GetButtonUp(Move)) {
+            if (Time.time - clickTime <= delay) {
+                // Click
+                IssueMove();
+            }
+        }
+        if (Input.GetButton(Move)) {
+            //if (Time.time - clickTime > delay) {
+                // Drag
+                IssueMove();
+            //}
+        }
 
-        GetComponent<CharacterController>().SimpleMove(direction);
-        //GetComponent<Rigidbody>().velocity = direction;
+        Vector3 direction = destination - transform.position;
+        direction.y = 0;
+        if (direction.magnitude > 2 * currentSpeed * Time.fixedDeltaTime) {
+            direction.Normalize();
+            direction.Scale(Vector3.one * currentSpeed);
+            currentSpeed += accel * Time.fixedDeltaTime;
+        }
+        else {
+            currentSpeed = direction.magnitude;
+
+            if (currentSpeed > maxSpeed)
+                currentSpeed = maxSpeed;
+        }
+
+        velocity = Vector3.Slerp(velocity, direction, 0.5f);
+        if (velocity.magnitude > maxSpeed) {
+            velocity.Normalize();
+            velocity *= maxSpeed;
+        }
+
+        GetComponent<CharacterController>().SimpleMove(velocity);
     }
 }
